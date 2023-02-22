cad190040d66a4ed295107d0c5931ea1570463fb
Artur Herdt <herdt.artur28@yahoo.com>
2023-02-22 13:32:51 +0100

    first commit

---
+++ src/main/java/com/example/alkoholrechner/AlkoholrechnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.alkoholrechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkoholrechnerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlkoholrechnerApplication.class, args);
+  }
+
+}
